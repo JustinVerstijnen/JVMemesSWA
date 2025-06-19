ef43e5ca189cb580135246f74e6ded93470e82f4
Justin Verstijnen <info@justinverstijnen.nl>
2025-06-19 09:27:14 +0200

    Add files via upload

---
+++ concertcounter/script.js
@@ -0,0 +1,21 @@
+function updateCountdowns() {
+  const now = new Date().getTime();
+  document.querySelectorAll('.concert-card').forEach(card => {
+    const date = new Date(card.dataset.datetime).getTime();
+    const distance = date - now;
+
+    if (distance <= 0) {
+      card.remove(); // verwijder kaart als concert is geweest
+      return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    card.querySelector('.countdown').innerText = `${days}d ${hours}u ${minutes}m`;
+  });
+}
+
+setInterval(updateCountdowns, 60000);
+window.onload = updateCountdowns;
